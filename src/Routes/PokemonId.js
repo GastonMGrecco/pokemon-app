c615c8cf95563319e209b60f715c0008d0a493b3
GastonMGrecco <gastonmgrecco@gmail.com>
2022-01-26 22:35:11 -0600

    mejora de css, casi todo

--- src/Routes/PokemonId.js
+++ src/Routes/PokemonId.js
@@ -17,10 +17,56 @@ useEffect(()=>{
 console.log(pokemon)
     
     return (
-        <div>
-            <h1>{id}</h1>
-            <img src={pokemon.sprites?.front_default} alt=""/>
-
+        <div className='TarjetaInd'style={
+            pokemon.types?.[0].type.name==="fighting"?
+            {backgroundColor:"orangered"}:
+            pokemon.types?.[0].type.name==="flying"?
+            {backgroundColor:"yellow"}:
+            pokemon.types?.[0].type.name==="poison"?
+            {backgroundColor:"purple"}:
+            pokemon.types?.[0].type.name==="ground"?
+            {backgroundColor:"brown"}:
+            pokemon.types?.[0].type.name==="rock"?
+            {backgroundColor:"mediumseagreen"}:
+            pokemon.types?.[0].type.name==="bug"?
+            {backgroundColor:"pink"}:
+            pokemon.types?.[0].type.name==="ghost"?
+            {backgroundColor:"black"}:
+            pokemon.types?.[0].type.name==='steel'?
+            {backgroundColor:"pink"}:
+            pokemon.types?.[0].type.name==='fire'?
+            {backgroundColor:"red"}:
+            pokemon.types?.[0].type.name==='water'?
+            {backgroundColor:"skyblue"}:
+            pokemon.types?.[0].type.name==='grass'?
+            {backgroundColor:"darkgreen"}:
+            pokemon.types?.[0].type.name==='electric'?
+            {backgroundColor:"yellow"}:
+            pokemon.types?.[0].type.name==='psichic'?
+            {backgroundColor:"peru"}:
+            pokemon.types?.[0].type.name==='ice'?
+            {backgroundColor:"red"}:
+            pokemon.types?.[0].type.name==='dragon'?
+            {backgroundColor:"khaki"}:
+            pokemon.types?.[0].type.name==='dark'?
+            {backgroundColor:"grey"}:
+            pokemon.types?.[0].type.name==='fairy'?
+            {backgroundColor:"pink"}:
+            pokemon.types?.[0].type.name==='unknown'?
+            {backgroundColor:"grey"}:
+            pokemon.types?.[0].type.name==='shadow'?
+            {backgroundColor:"black"}:{backgroundColor:"lightgoldenrodyellow"}
+        
+        }>
+            <h1 className='NombreTarInd'>{id}</h1>
+            <img className="ImagenGrande"src={pokemon.sprites?.other.dream_world.front_default} alt=""/>
+           <div>
+           <h2>Habilidades:</h2>
+            <p>{pokemon.abilities?.[0].ability.name}</p>
+            <p>{pokemon.abilities?.[1].ability.name}</p>
+            <h2>Pokemón tipo:</h2>
+            <p>{pokemon.types?.[0].type.name}</p>
+            </div>
         </div>
     );
 };
--- src/Routes/Tarjeta.js
+++ src/Routes/Tarjeta.js
@@ -14,54 +14,59 @@ const Tarjeta = ({url}) => {
 
     },[url])
     
- 
+ console.log(pok)
     
     return (
          <div className='tarjeta' style={
-            pok?.types[0]?.type?.name==="fighting"?
-            {backgroundColor:"orange"}:
-            pok?.types[0]?.type?.name==="flying"?
+            pok.types?.[0].type.name==="fighting"?
+            {backgroundColor:"orangered"}:
+            pok.types?.[0].type.name==="flying"?
             {backgroundColor:"yellow"}:
-            pok?.types[0]?.type?.name==="poison"?
+            pok.types?.[0].type.name==="poison"?
             {backgroundColor:"purple"}:
-            pok?.types[0]?.type?.name==="ground"?
+            pok.types?.[0].type.name==="ground"?
             {backgroundColor:"brown"}:
-            pok?.types[0]?.type?.name==="rock"?
-            {backgroundColor:"grey"}:
-            pok?.types[0]?.type?.name==="bug"?
-            {backgroundColor:"green"}:
-            pok?.types[0]?.type?.name==="ghost"?
+            pok.types?.[0].type.name==="rock"?
+            {backgroundColor:"mediumseagreen"}:
+            pok.types?.[0].type.name==="bug"?
+            {backgroundColor:"pink"}:
+            pok.types?.[0].type.name==="ghost"?
             {backgroundColor:"black"}:
-            pok?.types[0]?.type?.name==='steel'?
+            pok.types?.[0].type.name==='steel'?
             {backgroundColor:"pink"}:
-            pok?.types[0]?.type?.name==='fire'?
+            pok.types?.[0].type.name==='fire'?
             {backgroundColor:"red"}:
-            pok?.types[0]?.type?.name==='water'?
-            {backgroundColor:"blue"}:
-            pok?.types[0]?.type?.name==='grass'?
-            {backgroundColor:"violet"}:
-            pok?.types[0]?.type?.name==='electric'?
+            pok.types?.[0].type.name==='water'?
+            {backgroundColor:"skyblue"}:
+            pok.types?.[0].type.name==='grass'?
+            {backgroundColor:"darkgreen"}:
+            pok.types?.[0].type.name==='electric'?
             {backgroundColor:"yellow"}:
-            pok?.types[0]?.type?.name==='psichic'?
+            pok.types?.[0].type.name==='psichic'?
             {backgroundColor:"peru"}:
-            pok?.types[0]?.type?.name==='ice'?
+            pok.types?.[0].type.name==='ice'?
             {backgroundColor:"red"}:
-            pok?.types[0]?.type?.name==='dragon'?
-            {backgroundColor:"gray"}:
-            pok?.types[0]?.type?.name==='dark'?
+            pok.types?.[0].type.name==='dragon'?
+            {backgroundColor:"khaki"}:
+            pok.types?.[0].type.name==='dark'?
             {backgroundColor:"grey"}:
-            pok?.types[0]?.type?.name==='fairy'?
+            pok.types?.[0].type.name==='fairy'?
             {backgroundColor:"pink"}:
-            pok?.types[0]?.type?.name==='unknown'?
+            pok.types?.[0].type.name==='unknown'?
             {backgroundColor:"grey"}:
-            pok?.types[0]?.type?.name==='shadow'?
-            {backgroundColor:"black"}:{backgroundColor:"white"}
+            pok.types?.[0].type.name==='shadow'?
+            {backgroundColor:"black"}:{backgroundColor:"lightgoldenrodyellow"}
         
         } >
             <Link className='link' to={`/pokedex/${pok.name}/`}>
                 
                 <h2>{pok?.name}</h2>
-                <img src={pok?.sprites?.front_default}alt=""/>
+                <img className="imgPok" src={pok.sprites?.front_default}alt=""/>
+                <div>
+                    <p>Tipo: {pok.types?.[0].type.name}</p>
+                    
+                
+                </div>
                 
             </Link>
         </div>
